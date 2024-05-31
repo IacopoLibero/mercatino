0adb0103da60fae9909e5b0705df36a508a40750
iacopo <bernabei.iacopo@itismeucci.com>
2024-05-31 15:19:03 +0200

    script per rimuovere foto dal db e cartella upload

--- back-end/elimina_annuncio.php
+++ back-end/elimina_annuncio.php
@@ -7,6 +7,19 @@
         header("Location: ../index.php");
     }
     $id=$_POST['idAnnuncio'];
+
+    $sql="SELECT url_foto FROM Foto WHERE idAnnuncio='$id'";
+        $result = $conn->query($sql);
+        while($row = $result->fetch_assoc()) {
+            $path = $row['nomeFoto'];
+            if(file_exists($path)) {
+                unlink($path);
+            }
+        }
+
+    $sql="DELETE FROM Foto WHERE idAnnuncio='$id'";
+    $conn->query($sql);
+    
     $sql = "DELETE FROM Annuncio WHERE id =$id ";
     $conn->query($sql);
     header("Location: ../front-end/profile.php");
